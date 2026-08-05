1da1219759c5e5803916345cd17c97925c05a28d
Almonther Alwaili <montherwaili@gmail.com>
2026-08-05 11:46:03 +0400

    create lass RecursivePalindromeChecker and Prompt the user to enter a word using modern Java IO

---
+++ JavaTask9/JavaTask9/src/RecursivePalindromeChecker.java
@@ -0,0 +1,6 @@
+public class RecursivePalindromeChecker {
+    public static void main(String[] args) {
+        // Prompt the user to enter a word using modern Java IO
+        IO.print("Enter a word: ");
+        String word = IO.readln().trim();
+}
