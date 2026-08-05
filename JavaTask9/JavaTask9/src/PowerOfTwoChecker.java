158c3243e9a7ea73be055b72fc1715df186b71dd
Almonther Alwaili <montherwaili@gmail.com>
2026-08-05 10:55:55 +0400

    create main and Prompt the user to enter a positive integer using modern Java IO

--- JavaTask9/JavaTask9/src/PowerOfTwoChecker.java
+++ JavaTask9/JavaTask9/src/PowerOfTwoChecker.java
@@ -1,2 +1,6 @@
 public class PowerOfTwoChecker {
+    public static void main(String[] args) {
+        // Prompt the user to enter a positive integer using modern Java IO
+        IO.print("Enter a positive integer: ");
+        int number = Integer.parseInt(IO.readln().trim());
 }
