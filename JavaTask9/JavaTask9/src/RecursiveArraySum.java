fde0c6ad43e38737fd052c13adae4402357929ab
Almonther Alwaili <montherwaili@gmail.com>
2026-08-05 14:36:51 +0400

    Validate that the array size is positive and greater than 0

--- JavaTask9/JavaTask9/src/RecursiveArraySum.java
+++ JavaTask9/JavaTask9/src/RecursiveArraySum.java
@@ -4,6 +4,11 @@ public class RecursiveArraySum {
         // Prompt the user to enter the size of the array
         IO.print("Enter the number of elements in the array: ");
         int size = Integer.parseInt(IO.readln().trim());
-
+        
+        // Validate that the array size is positive and greater than 0
+        if (size <= 0) {
+            IO.println("Invalid size. Please enter a number greater than 0.");
+            return;
+        }
     }
 }
