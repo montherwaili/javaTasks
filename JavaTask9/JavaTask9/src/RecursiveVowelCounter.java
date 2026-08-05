a8e70f900df7c3ca46d3862fba38e71507d5d9ee
Almonther Alwaili <montherwaili@gmail.com>
2026-08-05 15:36:01 +0400

    Call the recursive method to count vowels

--- JavaTask9/JavaTask9/src/RecursiveVowelCounter.java
+++ JavaTask9/JavaTask9/src/RecursiveVowelCounter.java
@@ -4,6 +4,7 @@ public class RecursiveVowelCounter {
         // Prompt the user to enter a text string using modern Java IO
         IO.print("Enter a string: ");
         String text = IO.readln();
-
+// Call the recursive method to count vowels
+        int totalVowels = countVowels(text);
     }
 }
