a1112ef8476421dddf92dd73b706e5b78c844e1e
Almonther Alwaili <montherwaili@gmail.com>
2026-08-05 14:34:59 +0400

    create class RecursiveArraySum and Prompt the user to enter the size of the array

---
+++ JavaTask9/JavaTask9/src/RecursiveArraySum.java
@@ -0,0 +1,9 @@
+public class RecursiveArraySum {
+
+    public static void main(String[] args) {
+        // Prompt the user to enter the size of the array
+        IO.print("Enter the number of elements in the array: ");
+        int size = Integer.parseInt(IO.readln().trim());
+
+    }
+}
