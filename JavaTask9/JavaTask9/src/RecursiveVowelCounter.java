6d6975bce12ee3e8ed41d2793176a3a119045ca3
Almonther Alwaili <montherwaili@gmail.com>
2026-08-05 15:35:20 +0400

    create class RecursiveVowelCounter and Prompt the user to enter a text string using modern Java IO

---
+++ JavaTask9/JavaTask9/src/RecursiveVowelCounter.java
@@ -0,0 +1,9 @@
+public class RecursiveVowelCounter {
+
+    public static void main(String[] args) {
+        // Prompt the user to enter a text string using modern Java IO
+        IO.print("Enter a string: ");
+        String text = IO.readln();
+
+    }
+}
