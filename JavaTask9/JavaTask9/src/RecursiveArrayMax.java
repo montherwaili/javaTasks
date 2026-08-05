a99a579f80de6be3d62484f755738cef9b66908d
Almonther Alwaili <montherwaili@gmail.com>
2026-08-05 15:10:48 +0400

    create class RecursiveArrayMax and  Prompt the user to enter the size of the array

---
+++ JavaTask9/JavaTask9/src/RecursiveArrayMax.java
@@ -0,0 +1,7 @@
+public class RecursiveArrayMax {
+    public static void main(String[] args) {
+        // Prompt the user to enter the size of the array
+        IO.print("Enter the number of elements in the array: ");
+        int size = Integer.parseInt(IO.readln().trim());
+    }
+}
