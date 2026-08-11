2fdee1f44b6a03eca5b472609369eb0cb9f6dc90
Almonther Alwaili <montherwaili@gmail.com>
2026-08-11 15:16:36 +0400

    create interface Task 415

---
+++ JavaTask11/src/Task415/Payable.java
@@ -0,0 +1,8 @@
+package Task415;
+
+// Central interface for anyone generating income for the gym
+interface Payable {
+    void printAllInfo();
+    double monthlyTotal();
+    String getName();
+}
