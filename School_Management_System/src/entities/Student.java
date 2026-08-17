6e5dcaaa5e542dbbb35c1324b114f350c5a404f0
Almonther Alwaili <montherwaili@gmail.com>
2026-08-17 15:00:54 +0400

    create constructor

--- School_Management_System/src/entities/Student.java
+++ School_Management_System/src/entities/Student.java
@@ -9,4 +9,15 @@ public class Student extends Person {
     private double feeBalance;
     private List<String> subjects;
 
+    public Student(String id, String firstName, String lastName, String dateOfBirth,
+                   String gender, String phoneNumber, String email, String address,
+                   String gradeLevel, String enrollmentDate, double feeBalance){
+
+            // super tells the parent to build its part first
+            super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
+            this.gradeLevel = gradeLevel;
+            this.enrollmentDate = enrollmentDate;
+            setFeeBalance(feeBalance);   // check runs
+            this.subjects = new ArrayList<>();
+    }
 }
