ff6184d62b03fb09e3c3815d6e3b9cf6db4e63e0
Almonther Alwaili <montherwaili@gmail.com>
2026-08-17 15:28:12 +0400

    create attributes

--- School_Management_System/src/entities/Teacher.java
+++ School_Management_System/src/entities/Teacher.java
@@ -3,5 +3,10 @@ package entities;
 import java.util.ArrayList;
 import java.util.List;
 
+public class Teacher extends Person {
+    private String subject;
+    private int experienceYears;
+    private double salary;
+    private List<String> classesTaught;
     }
 }
