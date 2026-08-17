d9544641c44239fd44fe35bca4d437041b26f327
Almonther Alwaili <montherwaili@gmail.com>
2026-08-17 16:35:53 +0400

    create attributes

--- School_Management_System/src/entities/HeadTeacher.java
+++ School_Management_System/src/entities/HeadTeacher.java
@@ -2,3 +2,5 @@ package entities;
 
 // Second level of inheritance: Person -> Teacher -> HeadTeacher
 public class HeadTeacher extends Teacher {
+    private String department;
+    private int teachersManaged;
