507e30448eea99f27dc704a337bd76a02eec3e18
Almonther Alwaili <montherwaili@gmail.com>
2026-08-17 16:05:38 +0400

    add attributes

--- School_Management_System/src/entities/SeniorStudent.java
+++ School_Management_System/src/entities/SeniorStudent.java
@@ -2,3 +2,5 @@ package entities;
 
 // Second level of inheritance: Person -> Student -> SeniorStudent
 public class SeniorStudent extends Student {
+    private String major;
+    private double gpa;
