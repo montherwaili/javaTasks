63b50e07a8355a09702c318446f3c2a06ce0dfa1
Almonther Alwaili <montherwaili@gmail.com>
2026-08-17 14:56:31 +0400

    add Attributies

--- School_Management_System/src/entities/Student.java
+++ School_Management_System/src/entities/Student.java
@@ -4,5 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Student extends Person {
-    
+    private String gradeLevel;
+    private String enrollmentDate;
+    private double feeBalance;
+    private List<String> subjects;
+
 }
