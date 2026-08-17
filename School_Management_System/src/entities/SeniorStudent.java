0881595b7224f8171665a1b9e3b572625f052c86
Almonther Alwaili <montherwaili@gmail.com>
2026-08-17 16:05:59 +0400

    create constructor

--- School_Management_System/src/entities/SeniorStudent.java
+++ School_Management_System/src/entities/SeniorStudent.java
@@ -4,3 +4,8 @@ package entities;
 public class SeniorStudent extends Student {
     private String major;
     private double gpa;
+
+    public SeniorStudent(String id, String firstName, String lastName, String dateOfBirth,
+                         String gender, String phoneNumber, String email, String address,
+                         String gradeLevel, String enrollmentDate, double feeBalance,
+                         String major, double gpa) {
