b648204d4d081c384250b65a94cf8ec376e2c89c
Almonther Alwaili <montherwaili@gmail.com>
2026-08-17 16:46:16 +0400

    update interface Displayable

--- School_Management_System/src/interfaces/Displayable.java
+++ School_Management_System/src/interfaces/Displayable.java
@@ -1,9 +1,6 @@
 package interfaces;
 
 public interface Displayable {
-    // parent version of displayInfo()
-    void displayInfo();
-
-    // short one-line summary (from the Displayable interface)
-    String displaySummary();
+    void displayInfo();       // print full details
+    String displaySummary();  // return a short one-line summary
 }
