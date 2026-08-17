94dc0871472e7b829948271abed70963b71fcaa9
Almonther Alwaili <montherwaili@gmail.com>
2026-08-17 11:20:32 +0400

    create private fields

--- School_Management_System/src/entities/Person.java
+++ School_Management_System/src/entities/Person.java
@@ -2,5 +2,13 @@ package entities;
 
 import interfaces.Displayable;
 
-public class Person implements Displayable { 
+public class Person implements Displayable {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+    private String address;
 }
