59b4f2306e8351d53d54e4cc3dde8b68dff784f6
Almonther Alwaili <montherwaili@gmail.com>
2026-08-17 15:29:39 +0400

    constructor

--- School_Management_System/src/entities/Teacher.java
+++ School_Management_System/src/entities/Teacher.java
@@ -8,5 +8,15 @@ public class Teacher extends Person {
     private int experienceYears;
     private double salary;
     private List<String> classesTaught;
+
+    public Teacher(String id, String firstName, String lastName, String dateOfBirth,
+                   String gender, String phoneNumber, String email, String address,
+                   String subject, int experienceYears, double salary) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address);
+        this.subject = subject;
+        setExperienceYears(experienceYears);   // check runs
+        this.salary = salary;
+        this.classesTaught = new ArrayList<>();
+    }
     }
 }
