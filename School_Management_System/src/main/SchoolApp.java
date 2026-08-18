fa097233a3c266bd732ffd4f63a95e7f190981bf
Almonther Alwaili <montherwaili@gmail.com>
2026-08-18 16:46:20 +0400

    start running the class

--- School_Management_System/src/main/SchoolApp.java
+++ School_Management_System/src/main/SchoolApp.java
@@ -10,3 +10,10 @@ public class SchoolApp {
     // one service object for the whole app 
     private StudentService studentService = new StudentService();
     private InputHandler input = new InputHandler();
+
+    public static void main(String[] args) {
+        SchoolApp app = new SchoolApp();
+        app.loadSampleData();
+        app.run();
+    }
+
