5e06e4723c803a1509d62b80b2b0833c228b068f
Almonther Alwaili <montherwaili@gmail.com>
2026-08-18 15:26:32 +0400

    create class input handler

--- School_Management_System/src/utils/InputHandler.java
+++ School_Management_System/src/utils/InputHandler.java
@@ -1,4 +1,12 @@
 package utils;
+import java.util.Scanner;
 
 public class InputHandler {
+    private Scanner scanner = new Scanner(System.in);
+
+    public String getStringInput(String prompt) {
+        System.out.print(prompt + " ");
+        return scanner.nextLine();
+    }
+
 }
