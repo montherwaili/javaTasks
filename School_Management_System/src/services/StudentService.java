4be5af422d5f59a1cfee6fc0fda256180eb46629
Almonther Alwaili <montherwaili@gmail.com>
2026-08-18 16:23:31 +0400

    first way to add student

--- School_Management_System/src/services/StudentService.java
+++ School_Management_System/src/services/StudentService.java
@@ -11,3 +11,11 @@ import java.util.List;
 public class StudentService implements Manageable<Student>, Searchable<Student> {
     private List<Student> students = new ArrayList<>();
 
+    // OVERLOADING: three ways to add a student
+    public Student addStudent(String firstName, String lastName, String phone) {
+        String id = HelperUtils.generateId("STU");
+        Student s = new Student(id, firstName, lastName, "", "", phone, "", "",
+                "Grade 1", "2026-01-01", 0);
+        students.add(s);
+        return s;
+    }
