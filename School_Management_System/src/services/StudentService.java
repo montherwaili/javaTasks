0800cf323f7c889eaba1fbc7a8d30179cf461256
Almonther Alwaili <montherwaili@gmail.com>
2026-08-18 16:10:53 +0400

    create services

--- School_Management_System/src/services/StudentService.java
+++ School_Management_System/src/services/StudentService.java
@@ -1,4 +1,13 @@
 package services;
 
-public class StudentService {
-}
+import entities.Student;
+import interfaces.Manageable;
+import interfaces.Searchable;
+import utils.HelperUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentService implements Manageable<Student>, Searchable<Student> {
+    private List<Student> students = new ArrayList<>();
+
