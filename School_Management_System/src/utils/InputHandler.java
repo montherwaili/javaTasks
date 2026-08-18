501b7fe80ae7e7ccd2920286e4bd46acab4e0f7a
Almonther Alwaili <montherwaili@gmail.com>
2026-08-18 15:27:11 +0400

    OVERLOADING: get an int, or get an int inside a range

--- School_Management_System/src/utils/InputHandler.java
+++ School_Management_System/src/utils/InputHandler.java
@@ -9,4 +9,11 @@ public class InputHandler {
         return scanner.nextLine();
     }
 
+    // OVERLOADING: get an int, or get an int inside a range
+    public int getIntInput(String prompt) {
+        System.out.print(prompt + " ");
+        while (!scanner.hasNextInt()) {
+            System.out.print("Please type a whole number: ");
+            scanner.next();
+        }
 }
